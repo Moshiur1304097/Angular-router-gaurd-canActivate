d8ca8605d4de397cb6e6572f3724cf65b3f57617
Moshiur Rahman <moshiurrahman6674@gmail.com>
2021-11-20 00:38:55 +0600

    canLoad guard implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoadGuardGuard } from './auth/load-guard.guard';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -7,7 +8,9 @@ const routes: Routes = [
 {
   path: 'admin',
   loadChildren: () =>
-  import('./admin/admin.module').then((m) => m.AdminModule),},
+    import('./admin/admin.module').then((m) => m.AdminModule),
+  canLoad: [LoadGuardGuard],
+},
 
 {
   path:'',
--- src/app/auth/load-guard.guard.ts
+++ src/app/auth/load-guard.guard.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
+import { CanLoad, Route, Router, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoadGuardGuard implements CanLoad {
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+
+  constructor(private auth: AuthService, private router: Router){}
+  canLoad(): 
+      | Observable<boolean | UrlTree> 
+      | Promise<boolean | UrlTree> 
+      | boolean 
+      | UrlTree {
+    return this.auth.isLoggedIn().pipe(
+      map(isLoggedIn => isLoggedIn || this.router.createUrlTree([''])
+      )
+    );
   }
 }
