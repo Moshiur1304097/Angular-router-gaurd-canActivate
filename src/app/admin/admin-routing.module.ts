e33e7aefd539b50ef4f0bd534313cc5739644846
Moshiur Rahman <moshiurrahman6674@gmail.com>
2021-11-20 13:46:38 +0600

    preloading strategy implemented and auth folder refactored

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationGuard } from '../auth/authentication.guard';
+import { FormGuardGuard } from '../auth/form-guard.guard';
+import { PermissionsGuard } from '../auth/permissions.guard';
 import { AddProductComponent } from './add-product/add-product.component';
 import { AddUserComponent } from './add-user/add-user.component';
-import { AuthenticationGuard } from './auth/authentication.guard';
-import { FormGuardGuard } from './auth/form-guard.guard';
-import { PermissionsGuard } from './auth/permissions.guard';
 import { ListComponent } from './list/list.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
